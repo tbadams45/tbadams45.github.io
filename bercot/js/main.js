6f751178cc6550ae6bd6862406f7c003ecbebec6
Timothy Adams <tbadams45@gmail.com>
2017-07-02 22:53:05 -0400

    made a lot of progress

--- bercot/js/main.js
+++ bercot/js/main.js
@@ -1,16 +1,8 @@
 // Handles all parsing of the input file, builds Theme objects that contain
 // quotes for their particular Theme.
 class InputFileParser {
-	constructor() {}
-
-
-	containsAbbreviation(arr, abbr) {
-		for(var i = 0; i < arr.length; i++) {
-			if(arr[i].abbreviation === abbr) {
-				return true
-			}
-		}
-		return false
+	constructor() {
+		this.themes = [];
 	}
 
 	file(fileContents) {
@@ -18,17 +10,41 @@ class InputFileParser {
 		if(fileContents[0] === "\n") {
 			fileContents = fileContents.substring(1) // get rid of first 
 		}
-		var sliced = fileContents.split("\n\n")
-		console.log(sliced)
+		var sliced = fileContents.trim().split("\n\n")
 
-		return sliced
+		for(var i = 0; i < sliced.length; i++) {
+			var quotes = quote(sliced[i])
+			for(var j = 0; j < quotes.length; j++) {
+				pushQuote
+			}
+		}
 	}
 
+	// parses an entire quote
 	quote(quote) {
+		var firstLineBreak = quote.indexOf("\n")
+		var references = quote.substring(0, firstLineBreak)
+		quote = quote.substring(firstLineBreak)
 
+		// proxy for determining if there wasn't a proper line break 
+		// between two quotes
+		if(quote.includes("{") || quote.includes("}")) {
+			return undefined
+		}
+
+		var parsedQuotes = this.referenceLine(references)
+		if(parsedQuotes === undefined) {
+			return undefined
+		}
+
+		for(var i = 0; i < parsedQuotes.length; i++) {
+			parsedQuotes.content = quote
+		}
+
+		return parsedQuotes
 	}
 
-	// the line that holds the references obviously.
+	// the line that holds the references.
 	// e.g. "{Rom 9:28: Rom 13:10; Isa 10:22-23}"
 	referenceLine(str) {
 		var references = []
@@ -60,14 +76,7 @@ class InputFileParser {
 		return references
 	}
 
-	// Where a "reference" is, e.g. "1 Cor 14:5"
-	// Also possible:
-	//	"Jude 19"
-	// 	"Nicene Creed"
-	//  "Gen 2:3,5,9"
-	//  "Gen 2:3, 5, 9"
-	//  "Mt 15:13-19"
-	//
+	// Where a "reference" is, e.g. "1 Cor 14:5" or "Nicene Creed"
 	reference(ref) {
 		ref = ref.trim()
 
@@ -76,9 +85,9 @@ class InputFileParser {
 		var verseNumber = ""
 		var verseText = ""
 
-		for(var i = 0; i < books.length; i++) {
-			if(ref.includes(books[i].abbreviation)) {
-				var len = books[i].abbreviation.length
+		for(var i = 0; i < bibleBooks.books.length; i++) {
+			if(ref.includes(bibleBooks.books[i].abbreviation)) {
+				var len = bibleBooks.books[i].abbreviation.length
 
 				var chapterVerse = ref.substring(len).trim()
 				chapterVerse = chapterVerse.split(":")
@@ -89,29 +98,14 @@ class InputFileParser {
 				var chapter = Number(chapterVerse[0].trim())
 				var verseText = chapterVerse[1].trim()
 			
-
 				// find verseNumber
-				var verseNumber;
-				var onComma = verseText.indexOf(",")
-				var onHyphen = verseText.indexOf("-")
-
-				var higher = onComma > onHyphen ? onComma : onHyphen
-				var lower  = onComma < onHyphen ? onComma : onHyphen
-
-				if(higher === -1) { // neither "-"" nor ","" is present 
-					verseNumber = Number(verseText.trim())
-				}
-				else if (lower !== -1) { // both exist
-					verseNumber = Number(verseText.substring(0, lower).trim())
-				} else { // only one exists
-					verseNumber = Number(verseText.substring(0, higher).trim())
-				}
+				var vInfo = this.verse(verseText)
 
 				return new Quote(
-					books[i].name, 
-					books[i].abbreviation, 
+					bibleBooks.books[i].abbreviation, 
+					bibleBooks.books[i].name, 
 					chapter, 
-					verseNumber, 
+					vInfo.verseNumber, 
 					verseText)
 			}
 		}
@@ -119,6 +113,32 @@ class InputFileParser {
 		// it's not a book, but the name of a topic.
 		// So just create a Quote with that name.
 		return new Quote(ref.trim(), ref.trim(), null, null, null)
+	}
+
+	// a "verse" might be, for instance "15-16", "3", "3-5, 9-20, 15"
+	verse(verseText) {
+		var verseNumber;
+
+		var onComma = verseText.indexOf(",")
+		var onHyphen = verseText.indexOf("-")
+		
+		var higher = onComma > onHyphen ? onComma : onHyphen
+		var lower  = onComma < onHyphen ? onComma : onHyphen
+
+		if(higher === -1) { // neither "-"" nor ","" is present 
+			verseNumber = Number(verseText.trim())
+		}
+		else if (lower !== -1) { // both exist
+			verseNumber = Number(verseText.substring(0, lower).trim())
+		} 
+		else { // only one exists
+			verseNumber = Number(verseText.substring(0, higher).trim())
+		}
+
+		return {
+			verseNumber : verseNumber,
+			verseText : verseText
+		}
 	}
 }
 
@@ -136,7 +156,28 @@ class Theme {
 
 	// sort quotes array in order of chapter and then verseNumber
 	sortQuotes() {
-
+		if(!bibleBooks.containsAbbreviation(this.abbreviation)) {
+			// it's a topic, and we can't sort topics on chapters/verses 
+			// because they don't have any.
+			return
+		}
+		// see MDN documentation on array.sort()
+		this.quotes.sort(function(a, b) {
+			if(a.chapter < b.chapter) {
+				return -1;
+			}
+			else if(a.chapter > b.chapter) {
+				return 1;
+			}
+			else { // chapters equal
+				if(a.verseNumber <= b.verseNumber) {
+					return -1;
+				}
+				else {
+					return 1;
+				}
+			}
+		})
 	}
 
 	// generates markdown text used to write Theme file
@@ -145,9 +186,46 @@ class Theme {
 	}
 }
 
+class ThemeArray {
+	constructor() {
+		this.array = []
+	}
+
+	indexOfTheme(abbreviation) {
+		for(var i = 0; i < this.array.length; i++) {
+			if(this.array[i].abbreviation === abbreviation) {
+				return i
+			}
+		}
+		return -1
+	}
+
+	// automatically adds theme if needed
+	addQuote(quote) {
+		var index = this.indexOfTheme(abbreviation)
+
+		if(index === -1) {
+			// theme doesn't currently exist
+			var theme = new Theme(quote.abbreviation, quote.name)
+			theme.pushQuote(quote)
+			this.array.push(theme)
+		}
+		else {
+			// theme exists
+			this.array[index].pushQuote(quote)
+		}
+	}
+
+	sortEachTheme() {
+		for(var i = 0; i < this.array.length; i++) {
+			this.array[i].sortQuotes()
+		}
+	}
+}
+
 
 class Quote {
-	constructor(name, abbreviation, chapter, verseNumber, verseText) {
+	constructor(abbreviation, name, chapter, verseNumber, verseText) {
 		this.name = name
 		this.abbreviation = abbreviation
 		this.chapter = chapter
@@ -166,81 +244,105 @@ class Quote {
 	}
 }
 
-var books = [
-	{abbreviation: "Gen", name: "Genesis"},
-	{abbreviation: "Ex", name: "Exodus"},
-	{abbreviation: "Lev", name: "Leviticus"},
-	{abbreviation: "Num", name: "Numbers"},
-	{abbreviation: "Deut", name: "Deuteronomy"},
-	{abbreviation: "Josh", name: "Joshua"},
-	{abbreviation: "Judg", name: "Judges"},
-	{abbreviation: "Ruth", name: "Ruth"},
-	{abbreviation: "1 Sam", name: "1 Samuel"},
-	{abbreviation: "2 Sam", name: "2 Samuel"},
-	{abbreviation: "1 Kings", name: "1 Kings"},
-	{abbreviation: "2 Kings", name: "2 Kings"},
-	{abbreviation: "1 Chr", name: "1 Chronicles"},
-	{abbreviation: "2 Chr", name: "2 Chronicles"},
-	{abbreviation: "Ezra", name: "Ezra"},
-	{abbreviation: "Neh", name: "Nehemiah"},
-	{abbreviation: "Tob", name: "Tobit"},
-	{abbreviation: "Jdt", name: "Judith"},
-	{abbreviation: "Esth 1", name: "Esther"},
-	{abbreviation: "1 Macc", name: "Maccabees"},
-	{abbreviation: "2 Macc", name: "2 Maccabees"},
-	{abbreviation: "Job", name: "Job"},
-	{abbreviation: "Ps", name: "Psalms"},
-	{abbreviation: "Prov", name: "Proverbs"},
-	{abbreviation: "Eccl", name: "Ecclesiates"},
-	{abbreviation: "Song", name: "Song of Solomon"},
-	{abbreviation: "Cant", name: "Canticles"},
-	{abbreviation: "Wis", name: "Wisdom"},
-	{abbreviation: "Sir", name: "Sirach"},
-	{abbreviation: "Isa", name: "Isaiah"},
-	{abbreviation: "Jer", name: "Jeremiah"},
-	{abbreviation: "Lam", name: "Lamentations"},
-	{abbreviation: "Bar", name: "Baruch"},
-	{abbreviation: "Ezek", name: "Ezekiel"},
-	{abbreviation: "Dan", name: "Daniel"},
-	{abbreviation: "Hos", name: "Hosea"},
-	{abbreviation: "Joel", name: "Joel"},
-	{abbreviation: "Am", name: "Amos"},
-	{abbreviation: "Jon", name: "Jonah"},
-	{abbreviation: "Mic", name: "Micah"},
-	{abbreviation: "Nah", name: "Nahum"},
-	{abbreviation: "Hab", name: "Habakkuk"},
-	{abbreviation: "Zeph", name: "Zephaniah"},
-	{abbreviation: "Hag", name: "Haggai"},
-	{abbreviation: "Zech", name: "Zechariah"},
-	{abbreviation: "Mal", name: "Malachi"},
-	{abbreviation: "Mt", name: "Matthew"},
-	{abbreviation: "Mk", name: "Mark"},
-	{abbreviation: "Lk", name: "Luke"},
-	{abbreviation: "Jn", name: "John"},
-	{abbreviation: "Acts", name: "Acts of the Apostles"},
-	{abbreviation: "Rom", name: "Romans"},
-	{abbreviation: "1 Cor", name: "1 Corinthians"},
-	{abbreviation: "2 Cor", name: "2 Corinthians"},
-	{abbreviation: "Gal", name: "Galatians"},
-	{abbreviation: "Eph", name: "Ephesians"},
-	{abbreviation: "Phil", name: "Philippians"},
-	{abbreviation: "Col", name: "Colossians"},
-	{abbreviation: "1 Thess", name: "1 Thessalonians"},
-	{abbreviation: "2 Thess", name: "2 Thessalonians"},
-	{abbreviation: "1 Tim", name: "1 Timothy"},
-	{abbreviation: "2 Tim", name: "2 Timothy"},
-	{abbreviation: "Tit", name: "Titus"},
-	{abbreviation: "Philemon", name: "Philemon"},
-	{abbreviation: "Heb", name: "Hebrews"},
-	{abbreviation: "Jas", name: "James"},
-	{abbreviation: "1 Pet", name: "1 Peter"},
-	{abbreviation: "2 Pet", name: "2 Peter"},
-	{abbreviation: "1 Jn", name: "1 John"},
-	{abbreviation: "2 Jn", name: "2 John"},
-	{abbreviation: "3 Jn", name: "3 John"},
-	{abbreviation: "Jude", name: "Jude"},
-	{abbreviation: "Rev", name: "Revelation"}
-]
+class BibleBooks {
+	constructor() {
+		this.books = [
+			{abbreviation: "Gen", name: "Genesis"},
+			{abbreviation: "Ex", name: "Exodus"},
+			{abbreviation: "Lev", name: "Leviticus"},
+			{abbreviation: "Num", name: "Numbers"},
+			{abbreviation: "Deut", name: "Deuteronomy"},
+			{abbreviation: "Josh", name: "Joshua"},
+			{abbreviation: "Judg", name: "Judges"},
+			{abbreviation: "Ruth", name: "Ruth"},
+			{abbreviation: "1 Sam", name: "1 Samuel"},
+			{abbreviation: "2 Sam", name: "2 Samuel"},
+			{abbreviation: "1 Kings", name: "1 Kings"},
+			{abbreviation: "2 Kings", name: "2 Kings"},
+			{abbreviation: "1 Chr", name: "1 Chronicles"},
+			{abbreviation: "2 Chr", name: "2 Chronicles"},
+			{abbreviation: "Ezra", name: "Ezra"},
+			{abbreviation: "Neh", name: "Nehemiah"},
+			{abbreviation: "Tob", name: "Tobit"},
+			{abbreviation: "Jdt", name: "Judith"},
+			{abbreviation: "Esth 1", name: "Esther"},
+			{abbreviation: "1 Macc", name: "Maccabees"},
+			{abbreviation: "2 Macc", name: "2 Maccabees"},
+			{abbreviation: "Job", name: "Job"},
+			{abbreviation: "Ps", name: "Psalms"},
+			{abbreviation: "Prov", name: "Proverbs"},
+			{abbreviation: "Eccl", name: "Ecclesiates"},
+			{abbreviation: "Song", name: "Song of Solomon"},
+			{abbreviation: "Cant", name: "Canticles"},
+			{abbreviation: "Wis", name: "Wisdom"},
+			{abbreviation: "Sir", name: "Sirach"},
+			{abbreviation: "Isa", name: "Isaiah"},
+			{abbreviation: "Jer", name: "Jeremiah"},
+			{abbreviation: "Lam", name: "Lamentations"},
+			{abbreviation: "Bar", name: "Baruch"},
+			{abbreviation: "Ezek", name: "Ezekiel"},
+			{abbreviation: "Dan", name: "Daniel"},
+			{abbreviation: "Hos", name: "Hosea"},
+			{abbreviation: "Joel", name: "Joel"},
+			{abbreviation: "Am", name: "Amos"},
+			{abbreviation: "Jon", name: "Jonah"},
+			{abbreviation: "Mic", name: "Micah"},
+			{abbreviation: "Nah", name: "Nahum"},
+			{abbreviation: "Hab", name: "Habakkuk"},
+			{abbreviation: "Zeph", name: "Zephaniah"},
+			{abbreviation: "Hag", name: "Haggai"},
+			{abbreviation: "Zech", name: "Zechariah"},
+			{abbreviation: "Mal", name: "Malachi"},
+			{abbreviation: "Mt", name: "Matthew"},
+			{abbreviation: "Mk", name: "Mark"},
+			{abbreviation: "Lk", name: "Luke"},
+			{abbreviation: "Jn", name: "John"},
+			{abbreviation: "Acts", name: "Acts of the Apostles"},
+			{abbreviation: "Rom", name: "Romans"},
+			{abbreviation: "1 Cor", name: "1 Corinthians"},
+			{abbreviation: "2 Cor", name: "2 Corinthians"},
+			{abbreviation: "Gal", name: "Galatians"},
+			{abbreviation: "Eph", name: "Ephesians"},
+			{abbreviation: "Phil", name: "Philippians"},
+			{abbreviation: "Col", name: "Colossians"},
+			{abbreviation: "1 Thess", name: "1 Thessalonians"},
+			{abbreviation: "2 Thess", name: "2 Thessalonians"},
+			{abbreviation: "1 Tim", name: "1 Timothy"},
+			{abbreviation: "2 Tim", name: "2 Timothy"},
+			{abbreviation: "Tit", name: "Titus"},
+			{abbreviation: "Philemon", name: "Philemon"},
+			{abbreviation: "Heb", name: "Hebrews"},
+			{abbreviation: "Jas", name: "James"},
+			{abbreviation: "1 Pet", name: "1 Peter"},
+			{abbreviation: "2 Pet", name: "2 Peter"},
+			{abbreviation: "1 Jn", name: "1 John"},
+			{abbreviation: "2 Jn", name: "2 John"},
+			{abbreviation: "3 Jn", name: "3 John"},
+			{abbreviation: "Jude", name: "Jude"},
+			{abbreviation: "Rev", name: "Revelation"}
+		]
+	}
+
+	containsAbbreviation(abbr) {
+		for(var i = 0; i < this.books.length; i++) {
+			if(this.books[i].abbreviation === abbr) {
+				return true
+			}
+		}
+		return false
+	}
+
+	containsName(name) {
+		for(var i = 0; i < this.books.length; i++) {
+			if(this.books[i].name === name) {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+const bibleBooks = new BibleBooks()
 
 function handleFileUpload() {
 	var fileReader = new FileReader()
@@ -265,7 +367,8 @@ function handleFileUpload() {
 
 module.exports = {
 	InputFileParser : InputFileParser,
-	Theme : Theme,
-	Quote : Quote,
-	books : books
+	Theme           : Theme,
+	ThemeArray      : ThemeArray,
+	bibleBooks      : bibleBooks,
+	Quote           : Quote,
 }
--- bercot/test/test.js
+++ bercot/test/test.js
@@ -4,10 +4,10 @@ var shouldArrays = require('chai-arrays')
 chai.use(shouldArrays)
 var expect = chai.expect //actually call the function;
 
-describe("InputFileParser", function() {
+describe("InputFileParser Class", function() {
 	var parse = new main.InputFileParser()
 
-	describe("referenceLine()", function() {
+	describe("referenceLine() basics", function() {
 		it("should be of type arrray", function() {
 			var result = parse.referenceLine("{2 Cor 7:1; Lk 11:39; Mt 15:16-20; Mt 5:8}")
 			expect(result).to.be.array()
@@ -24,15 +24,15 @@ describe("InputFileParser", function() {
 
 			expect(commas.length).to.equal(4)
 			expect(commas[0]).to.deep.equal(
-				new main.Quote("Romans", "Rom", 4, 3, "3, 9, 22, 23"))
+				new main.Quote("Rom", "Romans", 4, 3, "3, 9, 22, 23"))
 		})
-		it("should handle commas AND hyphens", function() {
+		it("should handle commas AND hyphens in same list", function() {
 			var both = "{Gen 18:2, 23-33; Gen 19:1}"
 			both = parse.referenceLine(both)
 
 			expect(both.length).to.equal(2)
 			expect(both[0]).to.deep.equal(
-				new main.Quote("Genesis", "Gen", 18, 2, "2, 23-33"))
+				new main.Quote("Gen", "Genesis", 18, 2, "2, 23-33"))
 		})
 		it("should remove junk outside of curly braces", function() {
 			var king = " hi }{2 Kings 17:4}}}{ ???"
@@ -62,21 +62,32 @@ describe("InputFileParser", function() {
 
 			expect(space.length).to.equal(5)
 			expect(space[0]).to.deep.equal(
-				new main.Quote("Acts of the Apostles", "Acts", 7, 44, "44"))
+				new main.Quote("Acts", "Acts of the Apostles", 7, 44, "44"))
 
 			expect(space[1]).to.deep.equal(
-				new main.Quote("Hebrews", "Heb", 8, 5, "5"))
+				new main.Quote("Heb", "Hebrews", 8, 5, "5"))
 
 			expect(space[2]).to.deep.equal(
-				new main.Quote("Hebrews", "Heb", 9, 23, "23-24"))
+				new main.Quote("Heb", "Hebrews", 9, 23, "23-24"))
 
 			expect(space[3]).to.deep.equal(
-				new main.Quote("Numbers", "Num", 9, 15, "15"))
+				new main.Quote("Num", "Numbers", 9, 15, "15"))
 
 			expect(space[4]).to.deep.equal(
-				new main.Quote("Numbers", "Num", 17, 7, "7"))
+				new main.Quote("Num", "Numbers", 17, 7, "7"))
 
 		})
+		it("should handle one chapter books", function() {
+			var john = "{3 Jn 4}"
+			john = parse.referenceLine(john)
+
+			expect(john.length).to.equal(1)
+			expect(john[0]).to.deep.equal(
+				new main.Quote("3 Jn", "3 John", 0, 4, "4"))
+		})
+	})
+
+	describe("referenceLine() error handling", function() {
 		it("should return undefined when two colons are in the same reference", function() {
 			var mismatch = "{Rom 9:28: Rom 13:10; Isa 10:22-23}"
 			mismatch = parse.referenceLine(mismatch)
@@ -91,11 +102,136 @@ describe("InputFileParser", function() {
 		})
 	})
 
+	describe("quote() basics", function() {
+		it("should populate the quote.content property", function() {
+			var qt = "{1 Tim 1:3-15; 2 Tim 2:4}\nintense quote!"
+			var result = parse.quote(qt)
+			expect(result[0]).to.deep.equal(
+				new main.Quote("1 Tim", "1 Timothy", 1, 3, "3-15"))
+
+			expect(result[1]).to.deep.equal(
+				new main.Quote("2 Tim", "2 Timothy", 2, 4, "4"))
+		})
+
+		it("should return an array", function() {
+			var qt = "{1 Tim 1:3; 2 Tim 2:4}\nintense quote!"
+			var result = parse.quote(qt)
+			expect(result).to.be.array()
+			expect(result).to.be.ofSize(2)
+		})
+	})
+
 	// quote will handle the entire quote, including references
 	// return a Quote with content set.
 	// or tells the user that it doesn't work (if referencelist is undefined) for some reason and moves on
 	// maybe also helpful to test i it's a bad quote (e.g. if maybe the user didn't put two new lines between quotes)
-	describe("quote", function() {
+	describe("quote() error handling", function() {
+		it("should return undefined if curly braces are in quote body", function() {
+			var qt1 = "{2 Cor 7:4}\nThis is content{"
+			var qt2 = "{Gen 7:4}\nThis is content}"
 
+			var result1 = parse.quote(qt1)
+			expect(result1).to.equal(undefined)
+
+			var result2 = parse.quote(qt2)
+			expect(result2).to.equal(undefined)
+		})
+		it("should return undefined if referenceLine returns undefined", function() {
+			var bad = "{2 Cor 7:4: Rom 15:3}"
+			var result = parse.quote(bad)
+			expect(result).to.equal(undefined)
+		})
+	})
+
+/*	describe("file()", function() {
+
+	})*/
+})
+
+
+describe("Theme Class", function() {
+	describe("pushQuote()", function() {
+		var theme = new main.Theme("2 Cor", "2 Corinthians")
+		var quote = new main.Quote("2 Cor", "2 Corinthians", 1, 13, "13-16")
+		quote.setContent("this is example content\non two lines")
+		theme.pushQuote(quote)
+
+		it("should have length one", function() {
+			expect(theme.quotes.length).to.equal(1)
+		})
+		it("should leave the quote unchanged", function() {
+			expect(theme.quotes[0]).to.deep.equal(quote)
+		})
+	})
+
+	describe("sortQuotes()", function() {
+		it("should leave topic themes unchanged", function () {
+			var theme = new main.Theme("Nicene Creed", "Nicene Creed")
+
+			var quote1 = new main.Quote("Nicene Creed", "Nicene Creed", null, null, null)
+			quote1.setContent("Content for quote 1")
+			var quote2 = new main.Quote("Nicene Creed", "Nicene Creed", null, null, null)
+			quote2.setContent("Content for quote 2")
+
+			theme.pushQuote(quote1)
+			theme.pushQuote(quote2)
+			theme.sortQuotes()
+
+			expect(theme.quotes[0]).to.deep.equal(quote1)
+			expect(theme.quotes[1]).to.deep.equal(quote2)
+
+			var themeReversed = new main.Theme("Nicene Creed", "Nicene Creed")
+			themeReversed.pushQuote(quote2)
+			themeReversed.pushQuote(quote1)
+			themeReversed.sortQuotes()
+
+			expect(themeReversed.quotes[0]).to.deep.equal(quote2)
+			expect(themeReversed.quotes[1]).to.deep.equal(quote1)
+		})
+		it("should sort by chapter first", function() {
+			var theme = new main.Theme("Gen", "Genesis")
+			var quote1 = new main.Quote("Gen", "Genesis", 4, 2, "2-3")
+			var quote2 = new main.Quote("Gen", "Genesis", 2, 5, "5")
+
+			theme.pushQuote(quote1)
+			theme.pushQuote(quote2)
+			theme.sortQuotes()
+
+			expect(theme.quotes[0]).to.deep.equal(quote2)
+			expect(theme.quotes[1]).to.deep.equal(quote1)
+		})
+		it("should sort by verse if chapters are equal", function() {
+			var theme = new main.Theme("Ex", "Exodus")
+			var quote1 = new main.Quote("Ex", "Exodus", 2, 5, "2-3")
+			var quote2 = new main.Quote("Ex", "Exodus", 2, 2, "5")
+
+			theme.pushQuote(quote1)
+			theme.pushQuote(quote2)
+			theme.sortQuotes()
+
+			expect(theme.quotes[0]).to.deep.equal(quote2)
+			expect(theme.quotes[1]).to.deep.equal(quote1)
+		})
+	})
+})
+
+describe("ThemeArray Class", function() {
+	describe("indexOfTheme()", function() {
+		it("should return -1 if theme doesn't exist", function() {
+			var arr = new main.ThemeArray()
+			var result = arr.indexOfTheme("Gen")
+
+			expect(result).to.equal(-1)
+		})
+		it("should return index if theme exists", function() {
+			var arr = new main.ThemeArray()
+			arr.array[0] = new main.Theme("2 Cor", "2 Corinthians")
+			arr.array[1] = new main.Theme("Gal", "Galatians")
+			var result = arr.indexOfTheme("2 Cor")
+			expect(result).to.equal(0)
+
+			var result = arr.indexOfTheme("Gal")
+			expect(result).to.equal(1)
+		})
 	})
 })
